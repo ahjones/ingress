cee58515c9ce083c41b1d757a09fa2ffe759f695
Andrew Jones <me@andrewhjon.es>
2012-11-16 20:00:37 +0000

    Add cljs

---
+++ src/cljs/ingressinvites/index.cljs
@@ -0,0 +1,26 @@
+(ns ingressinvites.index
+  (:require [goog.events :as ev]
+            [goog.events.Event :as events]
+            [goog.net.XhrIo :as xhr]
+            [goog.dom :as dom]))
+
+(defn add-email-callback [message]
+  (set! (.-innerHTML (dom/getElement "thanks"))
+        "Thanks"))
+
+(defn get-dom-value
+  [id]
+  (.-value (dom/getElement id)))
+
+(defn add-email
+  [e]
+  (xhr/send "/hopeful/" add-email-callback "POST" (str "{\"email\":\"" (get-dom-value "email") "\"}"))
+
+  (.preventDefault e))
+
+(defn ^:export main []
+
+  (ev/listen
+   (dom/getElement "add-email-button")
+   "click"
+   add-email))
